f1c90f4039fed7f0e1f65776d196fa5f0920f189
Sam Legarreta <samlegarreta@gmail.com>
2019-08-13 01:24:13 -0400

    loop over array attempt

--- week_5_hw/2_react_http/src/App.js
+++ week_5_hw/2_react_http/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const APIKEY = '1d85e4833f8424524d92f74117ca0f3d';
+var city = ['Tehran', 'Barcelona', 'London']
 
 export default class App extends Component {
 	state = {
 		websiteName: 'Open Weather Map!',
 		data: {},
-		url: `http://api.openweathermap.org/data/2.5/weather?zip=20002,us&units=imperial&appid=${APIKEY}`,
+		url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKEY}`,
 	};
 
 	componentDidMount(){
@@ -15,6 +16,10 @@ export default class App extends Component {
 	}
 
 	makeRequest = () => {
+		for (let i = 0; i < city.length; i++){
+			// return this.state.url
+			console.log(this.state.url);
+		
 			fetch(this.state.url).then((response) => {
 				return response.json();
 			}).then((data) => {
@@ -23,7 +28,7 @@ export default class App extends Component {
 					data: data,
 				})
 			})
-
+		}
 	};
 
 	render() {
@@ -34,7 +39,7 @@ export default class App extends Component {
 				<button onClick={this.addItem} className="btn btn-primary">Search Zipcode</button>
 				<div className="card rounded shadow p-3">
 					<h1>{this.state.data.name}</h1>
-					<h4>Temp: {this.state.data.main && this.state.data.main.temp} {'\u00b0'}</h4>
+					<h4>Temperature: {this.state.data.main && this.state.data.main.temp} {'\u00b0'}</h4>
 					<p>Wind Speed: {this.state.data.wind && this.state.data.wind.speed} MPH</p>
 					<p>{this.state.data.weather && this.state.data.weather[0].description}</p>
 					<p><img src={`http://openweathermap.org/img/w/${this.state.data.weather && this.state.data.weather[0].icon}.png`} /></p>
